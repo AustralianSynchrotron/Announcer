d41cd5d508d4643a2a14553764f79dc6a5cdec53
RobbieClarken <robbie.clarken@gmail.com>
2013-06-04 09:59:28 +1000

    Initial Commit

---
+++ AS Announcer/ASLSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASLSAppDelegate.h
+//  AS Announcer
+//
+//  Created by Robbie Clarken on 4/06/13.
+//  Copyright (c) 2013 Robbie Clarken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASLSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AS Announcer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AS Announcer
+//
+//  Created by Robbie Clarken on 4/06/13.
+//  Copyright (c) 2013 Robbie Clarken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASLSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASLSAppDelegate class]));
+    }
+}
